e2129bccff623b5ede4f4dc8a2d898ebb608d437
HitenDhiman15 <hitendhiman15@gmail.com>
2025-07-28 22:09:34 +0530

    Added Lecture032

---
+++ Lecture032/climbStairs.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int countDistinctWays(int nStairs) {
+    //base case
+    if(nStairs < 0) {
+        return 0;
+    }
+    if(nStairs == 0) {
+        return 1;
+    }
+
+    //R.C.
+    int ans = countDistinctWays(nStairs - 1) + countDistinctWays(nStairs - 2);
+
+    return ans; 
+}
