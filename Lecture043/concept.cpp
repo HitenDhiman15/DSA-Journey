a5afab03281902c488cefb757318470ee9dc8173
HitenDhiman15 <hitendhiman15@gmail.com>
2025-08-11 23:44:44 +0530

    Added Lecture043

---
+++ Lecture043/concept.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student {
+
+    private:
+       string name;
+       int age;
+       int height;
+
+    public:
+    int getAge() {
+        return this->age;
+    }
+};
+
+int main() {
+
+    Student first;
+
+    cout << "Sabb sahi chalra hai" << endl;
+    
+    return 0;
+}
